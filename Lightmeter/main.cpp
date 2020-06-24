6f99a7174d7c9df577039a2e7ede9709945ae2c1
Milooooo1 <2002milo@gmail.com>
2020-06-24 14:50:54 +0200

    Cleaned up code
    Still not accuratly displaying values

--- Lightmeter/main.cpp
+++ Lightmeter/main.cpp
@@ -1,12 +1,11 @@
 #include "hwlib.hpp"
 #include "lightmeter.hpp"
-#include <string>
 
 bool displayed = false;
 int ISO = 0;
-float shutterSpeed = 0;                              //Supposed to be a float but gives problems
-float Apperature = 0;                              //Supposed to be a float but gives problems
-
+float shutterSpeed = 0;
+int realShutterspeed = 0;                             
+int Apperature = 0;                              //Supposed to be a float but gives problems
 
 void clearScreen(hwlib::terminal_from & display, hwlib::terminal_from & header){
     display << "\f" << hwlib::flush;
@@ -21,35 +20,37 @@ void showSettingsMenu(hwlib::terminal_from & display, hwlib::terminal_from & hea
     clearScreen(display, headerDisplay);
     headerDisplay << " Settings menu" << hwlib::flush;
     displayed = false;
-    std::string s = std::to_string(Apperature);
-    const char* apArray = s.c_str();
-    int realShutterspeed = 1 / shutterSpeed;
+    bool calculated = false;
+
     for(;;){
+
         if(!displayed){
             display << "\f" << hwlib::flush;
             display << "\n"
                 << "ISO: "  << ISO      << "\n"
                 << "t: 1/"  << realShutterspeed << "\n"
-                << "f/"     << apArray << hwlib::flush; 
+                << "f/"     << Apperature << hwlib::flush; 
         }
         displayed = true;
 
         sw1.refresh(); sw2.refresh(); sw3.refresh(); sw4.refresh();
-        if(sw1.read() == 0 && sw2.read() == 0){
+        hwlib::wait_ms(10);
+        if(sw1.read() == 0 && calculated){
+            displayed = false;
             return;
         } else if (sw2.read() == 0 && sw1.read() == 1 && sw3.read() == 1 && sw4.read() == 1){ //ISO
             switch (ISOcounter) {
-                case 0: ISO = 0; break;
-                case 1: ISO = 100; break;
-                case 2: ISO = 125; break;
-                case 3: ISO = 160; break;
-                case 4: ISO = 200; break;
-                case 5: ISO = 250; break;
-                case 6: ISO = 320; break;
-                case 7: ISO = 400; break;
-                case 8: ISO = 500; break;
-                case 9: ISO = 640; break;
-                case 10: ISO = 800; break;
+                case 0:  ISO = 0;    break;
+                case 1:  ISO = 100;  break;
+                case 2:  ISO = 125;  break;
+                case 3:  ISO = 160;  break;
+                case 4:  ISO = 200;  break;
+                case 5:  ISO = 250;  break;
+                case 6:  ISO = 320;  break;
+                case 7:  ISO = 400;  break;
+                case 8:  ISO = 500;  break;
+                case 9:  ISO = 640;  break;
+                case 10: ISO = 800;  break;
                 case 11: ISO = 1000; break;
                 case 12: ISO = 1250; break;
                 case 13: ISO = 1600; break;
@@ -64,109 +65,98 @@ void showSettingsMenu(hwlib::terminal_from & display, hwlib::terminal_from & hea
             ISOcounter++;
             displayed = false;
             continue;
-        } else if (sw3.read() == 0 && sw2.read() == 1 && sw1.read() == 1 && sw4.read() == 1){ //Aperature
+        } else if (sw4.read() == 0 && sw2.read() == 1 && sw1.read() == 1 && sw3.read() == 1){ //Aperature
             switch (APcounter) {
-                case 0: Apperature = 0; break;
-                case 1: Apperature = 1.1; break;
-                case 2: Apperature = 1.2; break;
-                case 3: Apperature = 1.4; break;
-                case 4: Apperature = 1.8; break;
-                case 5: Apperature = 2.0; break;
-                case 6: Apperature = 2.8; break;
-                case 7: Apperature = 4.0; break;
-                case 8: Apperature = 5.6; break;
-                case 9: Apperature = 6.3; break;
+                case 0:  Apperature = 0;   break;
+                case 1:  Apperature = 1.1; break;
+                case 2:  Apperature = 1.2; break;
+                case 3:  Apperature = 1.4; break;
+                case 4:  Apperature = 1.8; break;
+                case 5:  Apperature = 2.0; break;
+                case 6:  Apperature = 2.8; break;
+                case 7:  Apperature = 4.0; break;
+                case 8:  Apperature = 5.6; break;
+                case 9:  Apperature = 6.3; break;
                 case 10: Apperature = 8.0; break;
-                case 11: Apperature = 11; break;
-                case 12: Apperature = 16; break;
-                case 13: Apperature = 22; break;
-                default: APcounter = 0; break;
+                case 11: Apperature = 11;  break;
+                case 12: Apperature = 16;  break;
+                case 13: Apperature = 22;  break;
+                default: APcounter  = 0;   break;
             }
             APcounter++;
             displayed = false;
             continue;
-        } else if (sw4.read() == 0 && sw2.read() == 1 && sw3.read() == 1 && sw4.read() == 1){ //Shutterspeed
+        } else if (sw3.read() == 0 && sw2.read() == 1 && sw4.read() == 1 && sw1.read() == 1){ //Shutterspeed
             switch (SScounter) {
-                case 0: shutterSpeed = 0; break;
-                case 1: shutterSpeed = 8000; break;
-                case 2: shutterSpeed = 6400; break;
-                case 3: shutterSpeed = 5000; break;
-                case 4: shutterSpeed = 4000; break;
-                case 5: shutterSpeed = 3200; break;
-                case 6: shutterSpeed = 2500; break;
-                case 7: shutterSpeed = 2000; break;
-                case 8: shutterSpeed = 1600; break;
-                case 9: shutterSpeed = 1250; break;
-                case 10: shutterSpeed = 1000; break;
-                case 11: shutterSpeed = 800; break;
-                case 12: shutterSpeed = 640; break;
-                case 13: shutterSpeed = 500; break;
-                case 14: shutterSpeed = 400; break;
-                case 15: shutterSpeed = 320; break;
-                case 16: shutterSpeed = 250; break;
-                case 17: shutterSpeed = 200; break;
-                case 18: shutterSpeed = 160; break;
-                case 19: shutterSpeed = 125; break;
-                case 20: shutterSpeed = 100; break;
-                case 21: shutterSpeed = 80; break;
-                case 22: shutterSpeed = 60; break;
-                case 23: shutterSpeed = 50; break;
-                case 24: shutterSpeed = 40; break;
-                case 25: shutterSpeed = 30; break;
-                case 26: shutterSpeed = 25; break;
-                case 27: shutterSpeed = 20; break;
-                case 28: shutterSpeed = 15; break;
-                case 29: shutterSpeed = 13; break;
-                case 30: shutterSpeed = 10; break;
-                case 31: shutterSpeed = 8; break;
-                case 32: shutterSpeed = 6; break;
-                case 33: shutterSpeed = 5; break;
-                case 34: shutterSpeed = 4; break;
-                default: SScounter = 0; break;
+                case 0:  realShutterspeed = 0;    break;
+                case 1:  realShutterspeed = 8000; break;
+                case 2:  realShutterspeed = 6400; break;
+                case 3:  realShutterspeed = 5000; break;
+                case 4:  realShutterspeed = 4000; break;
+                case 5:  realShutterspeed = 3200; break;
+                case 6:  realShutterspeed = 2500; break;
+                case 7:  realShutterspeed = 2000; break;
+                case 8:  realShutterspeed = 1600; break;
+                case 9:  realShutterspeed = 1250; break;
+                case 10: realShutterspeed = 1000; break;
+                case 11: realShutterspeed = 800;  break;
+                case 12: realShutterspeed = 640;  break;
+                case 13: realShutterspeed = 500;  break;
+                case 14: realShutterspeed = 400;  break;
+                case 15: realShutterspeed = 320;  break;
+                case 16: realShutterspeed = 250;  break;
+                case 17: realShutterspeed = 200;  break;
+                case 18: realShutterspeed = 160;  break;
+                case 19: realShutterspeed = 125;  break;
+                case 20: realShutterspeed = 100;  break;
+                case 21: realShutterspeed = 80;   break;
+                case 22: realShutterspeed = 60;   break;
+                case 23: realShutterspeed = 50;   break;
+                case 24: realShutterspeed = 40;   break;
+                case 25: realShutterspeed = 30;   break;
+                case 26: realShutterspeed = 25;   break;
+                case 27: realShutterspeed = 20;   break;
+                case 28: realShutterspeed = 15;   break;
+                case 29: realShutterspeed = 13;   break;
+                case 30: realShutterspeed = 10;   break;
+                case 31: realShutterspeed = 8;    break;
+                case 32: realShutterspeed = 6;    break;
+                case 33: realShutterspeed = 5;    break;
+                case 34: realShutterspeed = 4;    break;
+                default: SScounter        = 0;    break;
         }
             SScounter++;
             displayed = false;
             continue;
         } else if (sw1.read() == 0 && sw2.read() == 1 && sw3.read() == 1 && sw4.read() == 1){
-            if(ISO == 0 && shutterSpeed != 0 && Apperature != 0){
-                ISO = meter.getISO((float)Apperature, shutterSpeed);
-                display << "\f" << hwlib::flush;
-                display << "ISO: " << ISO << hwlib::flush;
+            if(ISO == 0 && realShutterspeed != 0 && Apperature != 0){
+                hwlib::cout << "\n Calculating ISO with values: \n Aperature: " << Apperature << "\n shutterspeed: " << realShutterspeed;
+                ISO = meter.getISO((float)Apperature, 1 / realShutterspeed);
+                hwlib::cout << "\n Result is ISO: " << ISO;
                 displayed = false;
-                hwlib::wait_ms(100);
-                sw1.refresh();
-                if(sw1.read() == 0){
-                    continue;
-                }
-            } else if(shutterSpeed == 0 && ISO != 0 && Apperature != 0){
+                calculated = true;
+                hwlib::wait_ms(50);
+            } else if(realShutterspeed == 0 && ISO != 0 && Apperature != 0){
+                hwlib::cout << "\n Calculating Shutterspeed with values: \n Aperature: " << Apperature << "\n ISO: " << ISO;
                 shutterSpeed = meter.getShutterspeed((float)Apperature, ISO);
-                display << "\f" << hwlib::flush;
-                display << "Shutterspeed: " << realShutterspeed << hwlib::flush;
+                realShutterspeed = 1 / shutterSpeed;
+                hwlib::cout << "\n Result is Shutterspeed: " << realShutterspeed;
                 displayed = false;
-                hwlib::wait_ms(100);
-                sw1.refresh();
-                if(sw1.read() == 0){
-                    continue;
-                }
-            } else if(Apperature == 0 && shutterSpeed != 0 && ISO != 0){
-                Apperature = meter.getApperature(ISO, shutterSpeed);
-                display << "\f" << hwlib::flush;
-                display << "Apperature: f/" << apArray << hwlib::flush;
+                calculated = true;
+                hwlib::wait_ms(50);
+            } else if(Apperature == 0 && realShutterspeed != 0 && ISO != 0){
+                hwlib::cout << "\n Calculating Aperature with values: \n ISO: " << ISO << "\n shutterspeed: " << realShutterspeed;
+                Apperature = meter.getApperature(ISO, 1 / realShutterspeed);
+                hwlib::cout << "\n Result is Aperature: " << Apperature;
                 displayed = false;
-                hwlib::wait_ms(100);
-                sw1.refresh();
-                if(sw1.read() == 0){
-                    continue;
-                }
+                calculated = true;
+                hwlib::wait_ms(50);
             } else {
                 display << "\f" << hwlib::flush;
                 display << "\nInvalid entry"<< hwlib::flush;
                 displayed = false;
-                hwlib::wait_ms(100);
-                sw1.refresh();
-                if(sw1.read() == 0){
-                    continue;
-                }
+                calculated = false;
+                hwlib::wait_ms(1000);
             }
         }
     }
